6205852f20f4a0bf20fe66b25386b55e68a3f421
Vadnais, Philippe (DFO MPO) <Philippe.Vadnais@dfo-mpo.gc.ca>
2025-02-27 13:37:49 -0500

    Add form helper

---
+++ Foundation.Components/TagHelpers/FDCP/FDCPFormTagHelper.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Foundation.Components.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Foundation.Components.TagHelpers.FDCP
+{
+    [HtmlTargetElement("fdcp-form", Attributes = "for, method, action")]
+    public class FDCPFormTagHelper : TagHelper
+    {
+        [HtmlAttributeName("for")]
+        public BaseViewModel Model { get; set; }
+
+        [HtmlAttributeName("method")]
+        public string Method { get; set; } = "post";
+
+        [HtmlAttributeName("action")]
+        public string Action { get; set; }
+
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            if (Model == null)
+            {
+                throw new ArgumentNullException(nameof(Model), "The model cannot be null.");
+            }
+
+            var childContent = output.Content.IsModified ? output.Content.GetContent() :
+            (await output.GetChildContentAsync()).GetContent();
+
+            // Start the <form> tag
+            output.TagName = "form";
+            output.Attributes.SetAttribute("method", Method);
+            if (!string.IsNullOrEmpty(Action))
+            {
+                output.Attributes.SetAttribute("action", Action);
+            }
+
+
+            var errorSummaryTag = new TagBuilder("gcds-error-summary");
+
+            // Add error summary if model has errors
+            if (!Model.IsValid)
+            {
+                var errorLinks = Model.Errors.ToDictionary(
+                    kvp => $"#{kvp.Key}", // Convert field names to anchor links
+                    kvp => string.Join(" ", kvp.Value) // Join multiple errors per field
+                );
+
+                var errorJson = JsonSerializer.Serialize(errorLinks);
+                errorSummaryTag.Attributes.Add("error-links", errorJson);
+            }
+
+            output.Content.AppendHtml(errorSummaryTag);
+
+            output.Content.AppendHtml(childContent);
+        }
+    }
+}
