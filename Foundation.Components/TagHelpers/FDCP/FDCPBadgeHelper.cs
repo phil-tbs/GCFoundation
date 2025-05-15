47694955b321652cbc7886f30b5226acd0eb404c
Vadnais, Philippe (DFO MPO) <Philippe.Vadnais@dfo-mpo.gc.ca>
2025-05-15 14:45:39 -0400

    add seach component and badges

---
+++ Foundation.Components/TagHelpers/FDCP/FDCPBadgeHelper.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Foundation.Components.TagHelpers.FDCP
+{
+    [HtmlTargetElement("fdcp-badge")]
+    public class FDCPBadgeHelper: TagHelper
+    {
+
+        public string Text { get; set; }
+
+        public bool IsRemovable { get; set; }
+
+        public string TagId { get; set; }
+
+        public FDCPBadgeStyle Style { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagMode = TagMode.StartTagAndEndTag;
+            output.TagName = "span";
+
+            var classes = new StringBuilder("fdcp-badge");
+            classes.Append($" fdcp-badge-{Style.ToString().ToLowerInvariant()}");
+
+            if (IsRemovable)
+            {
+                classes.Append(" fdcp-badge-removable");
+            }
+
+            output.Attributes.SetAttribute("class", classes.ToString());
+
+            if (!string.IsNullOrEmpty(TagId))
+            {
+                output.Attributes.SetAttribute("id", TagId);
+            }
+
+            // Build inner HTML
+            var contentBuilder = new StringBuilder();
+            contentBuilder.Append(Text);
+
+            if (IsRemovable)
+            {
+                contentBuilder.Append(" ");
+                contentBuilder.Append("<button type='button' class='fdcp-badge-close' aria-label='Remove badge'>&times;</button>");
+            }
+
+            output.Content.SetHtmlContent(contentBuilder.ToString());
+
+        }
+
+    }
+
+    public enum FDCPBadgeStyle
+    {
+        Success,
+        Danger,
+        Info,
+        Warning,
+        Primary,
+        Secondary,
+        Light,
+        Dark
+    }
+}
---
+++ Foundation.Components/TagHelpers/FDCP/FDCPSearchBoxTagHelper.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Foundation.Components.TagHelpers.FDCP
+{
+    [HtmlTargetElement("fdcp-search-box")]
+    public class FDCPSearchBoxTagHelper: TagHelper
+    {
+        public required string Placeholder { get; set; }
+
+        public required string Label { get; set; }
+
+        public string Name { get; set; }
+
+        public required string SearchBoxId { get; set; }
+
+        public string? Value { get; set; }
+
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagMode = TagMode.StartTagAndEndTag;
+            output.TagName = "div";
+            output.Attributes.SetAttribute("class","fdcp-filtered-search");
+
+            StringBuilder sr = new StringBuilder();
+
+            sr.Append("<div class='fdcp-search-box-wrapper'>");
+
+            //sr.Append("<span class='search-icon'></span>");
+            sr.Append(CultureInfo.InvariantCulture, $"<label class='sr-only'>{Label}</label>");
+            sr.Append(CultureInfo.InvariantCulture, $"<input type='search' {(string.IsNullOrEmpty(Value)? "": $"value='{Value}'")} placeholder='{Placeholder}'>");
+            sr.Append("</div>");
+
+            output.Content.SetHtmlContent(sr.ToString());
+        }
+
+    }
+}
