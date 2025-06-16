5776dd28bbda109b24b4189c3d2ca75a1ba4594e
Vadnais, Philippe (DFO MPO) <Philippe.Vadnais@dfo-mpo.gc.ca>
2025-06-16 09:59:49 -0400

    Make sure to clear the validation after required changed

--- Foundation.Components/wwwroot/src/js/components/fdcp-formBuilder.js
+++ Foundation.Components/wwwroot/src/js/components/fdcp-formBuilder.js
@@ -155,38 +155,32 @@ const applyDependencyAction = (element, action, shouldApply, dependency) => {
         case 1: // Show
             toggleVisibility(wrapper, shouldApply);
             if (shouldApply) {
-                // reinitializeGCDSComponent(element);
             }
             break;
             
         case 2: // Hide
             toggleVisibility(wrapper, !shouldApply);
             if (!shouldApply) {
-                // reinitializeGCDSComponent(element);
             }
             break;
             
         case 3: // Enable
             toggleDisabled(element, !shouldApply);
-            // reinitializeGCDSComponent(element);
             break;
             
         case 4: // Disable
             toggleDisabled(element, shouldApply);
-            // reinitializeGCDSComponent(element);
             break;
             
         case 5: // ClearValue
             if (shouldApply) {
                 clearElementValue(element);
-                // reinitializeGCDSComponent(element);
             }
             break;
             
         case 6: // SetValue
             if (shouldApply && dependency.setValue) {
                 setElementValue(element, dependency.setValue);
-                // reinitializeGCDSComponent(element);
             }
             break;
             
@@ -288,8 +282,9 @@ const toggleDisabled = (element, disabled) => {
 const setRequired = (element, required) => {
     if (!element) return;
     try {
-
         const clone = element.cloneNode(true);
+
+        clone.removeAttribute('error-message');
 
         if (clone.tagName.toLowerCase().startsWith('gcds-')) {
             if (required) {
